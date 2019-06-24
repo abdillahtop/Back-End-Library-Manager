d8d515b8eafd793ffdd4e6412f5a84ec37d20a4f
abdillahtop <abdillahtop@gmail.com>
2019-06-24 13:13:12 +0700

    update app

--- app.js
+++ app.js
@@ -26,11 +26,20 @@ app.post('/',(req,res) => {
         name : req.body.name,
         writter : req.body.writter,
         location : req.body.location,
-        category : req.body.category
+        category_id : req.body.category_id
     }
+
     conn.query('INSERT INTO lantai1 SET ?', [data], (err,result) => {
-        if (err) console.log(err)
-        res.json(result)
+        if (err){
+            return res.send({
+                error : err,
+            })
+        } else {
+            return res.send({
+                status : 200,
+                message :`Nama : ${data.name}, Writter : ${data.writter}, Location : ${location} has been insert`
+            })
+        }
     })
 })
 
@@ -41,28 +50,53 @@ app.patch('/:userid', (req,res) => {
         name : req.body.name,
         writter : req.body.writter,
         location : req.body.location,
-        category : req.body.category
+        category_id : req.body.category_id
     }
 
     conn.query('UPDATE lantai1 SET ? WHERE id=?', [data,userid], (err,result) => {
-        if(err) console.log(err)
-        res.json(result)
+        if (err){
+            return res.send({
+                error : err,
+            })
+        } else {
+            return res.send({
+                status : 200,
+                message :`data updated on Title :${data.name}, Writter : ${data.writter}, Location : ${data.location} `
+            })
+        }
+
     })
 
 })
 
 app.delete('/:userid', (req,res) => {
     const userid = req.params.userid
+    const data = {
+        name : req.body.name,
+        writter : req.body.writter,
+        location : req.body.location,
+        category_id : req.body.category_id
+    }
 
     conn.query('DELETE FROM lantai1 WHERE id=?' , [userid], (err,result) => {
-        if(err) console.log(err)
-        res.json(result)
+        if (err){
+            return res.send({
+                error : err,
+            })
+        } else {
+            return res.send({
+                status : 200,
+                message :`Title : ${data.name},Writter by : ${data.writter} at: ${data.location} has been Delete`,
+                result : result
+            })
+        }
+
     })
 })
 
 app.get('/all', (req,res) => {
 
-    conn.query(`SELECT * FROM lantai1`, (err,result) => {
+    conn.query(`SELECT lt1.id,name,writter,location,lt2.category FROM lantai1 lt1 JOIN lantai2 lt2 ON lt1.category_id = lt2.category_id`, (err,result) => {
         if(err) console.log(err)
         res.json(result)
     })
@@ -71,7 +105,7 @@ app.get('/all', (req,res) => {
 app.get('/', (req,res) => {
     const search = req.query.search
 
-    conn.query(`SELECT * FROM lantai1 WHERE category LIKE '%${search}%' OR location LIKE '%${search}%' `, (err,result) => {
+    conn.query(`SELECT lt1.id,name,writter,location,lt2.category FROM lantai1 lt1 JOIN lantai2 lt2 ON lt1.category_id = lt2.category_id WHERE category LIKE '%${search}%' OR location LIKE '%${search}%' `, (err,result) => {
         if(err) console.log(err)
         res.json(result)
     })
